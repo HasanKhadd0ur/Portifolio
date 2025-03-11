4180cda9c827de8df4c4535ff6c3382c96c90f45
Hasan Khaddour <hasan.b.khaddour@gmail.com>
2025-03-11 22:06:20 +0300

    Add Project Config

---
+++ src/configs/app.config.ts
@@ -0,0 +1,14 @@
+export  interface AppConfig {
+    userName : string ,
+    email : string,
+    logo:string,
+    fullName : string
+}
+
+export const  DefaultConfig : AppConfig={
+    userName :"Hasan Khaddour",
+    email:"Hasan.b.khaddour@gmail.com",
+    fullName:"Hasan Khaddour",
+    logo:'favicon.png'
+
+}
